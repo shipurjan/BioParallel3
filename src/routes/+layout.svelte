d96ac14b555bd539033a03b7eaab53b5f81a0242
Cyprian Zdebski <cyprianz5mail@gmail.com>
2023-09-16 23:42:20 +0200

    feat: add Lato font family; set minimum window size to 1280x720

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
   import '../app.css';
+  import { appWindow, PhysicalSize } from '@tauri-apps/api/window';
+  void appWindow.setMinSize(new PhysicalSize(1280, 720));
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,8 @@
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
-
-<script>
-  import Greet from '../lib/Greet.svelte';
-</script>
-
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>some paragraph</p>
-<Greet />
+<div
+  class="flex flex-col items-center align-center p-2 gap-2 w-[100dvw] h-[100dvh]"
+>
+  <h1 class="text-3xl font-light">Welcome to SvelteKit</h1>
+  <h1 class="text-3xl font-normal">Welcome to SvelteKit</h1>
+  <h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
+  <h1 class="text-3xl font-black">Welcome to SvelteKit</h1>
+</div>
