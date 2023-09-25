fe53c78a418bbd14c11e4e11b34ad96eb0a83576
Cyprian Zdebski <cyprianz5mail@gmail.com>
2023-09-25 22:36:33 +0200

    feat(canvas.svelte): add a basic canvas container

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -1,1 +1,15 @@
-<p>canvas</p>
+<script lang="ts">
+  import * as PIXI from 'pixi.js';
+  import { onMount } from 'svelte';
+
+  let container: HTMLDivElement;
+  onMount(() => {
+    const app = new PIXI.Application<HTMLCanvasElement>({
+      background: '#1099bb',
+      resizeTo: container,
+    });
+    container.appendChild(app.view);
+  });
+</script>
+
+<div class="h-[500px] w-[500px]" bind:this="{container}"></div>
