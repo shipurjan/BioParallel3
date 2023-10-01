c8b5c6a179144a7407385519d53fc698cad5a5ff
Cyprian Zdebski <cyprianz5mail@gmail.com>
2023-10-01 02:21:51 +0200

    refactor(main.rs): remove unused code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,7 @@ use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![show_window, read_file, handle_dropped_files])
+        .invoke_handler(tauri::generate_handler![show_window, read_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -27,13 +27,4 @@ async fn read_file(path: std::path::PathBuf) -> Result<Vec<u8>, String> {
       Ok(data) => Ok(data),
       Err(err) => Err(format!("Failed to read file: {}", err)),
   }
-}
-
-#[tauri::command]
-fn handle_dropped_files(files: Vec<std::path::PathBuf>) {
-    for file_path in files {
-        // Now, `file_path` contains the absolute path of the dropped file
-        println!("Dropped file path: {:?}", file_path);
-        // You can perform further actions with the absolute file path
-    }
 }
